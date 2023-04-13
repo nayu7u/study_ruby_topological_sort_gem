6bccf7c48e2451f8d0cb42af2c2fb0dfe4ffbec3
nayu7u <19748682+nayu7u@users.noreply.github.com>
2023-04-14 00:27:10 +0900

    test: add topological sort test

--- test/topological_sort/test_topological_sort.rb
+++ test/topological_sort/test_topological_sort.rb
@@ -1,8 +1,51 @@
 require "test_helper"
 
 class TestTopologicalSort
-  def test_exec
-    result = TestStudyRubyTopologicalSortGem::TestTopologicalSort.exec
-    assert(result == "foo")
+  def test_exec1
+    graph = [
+      [0, 5],
+      [1, 3],
+      [1, 6],
+      [2, 5],
+      [2, 7],
+      [3, 0],
+      [3, 7],
+      [4, 1],
+      [4, 2],
+      [4, 6],
+      [6, 7],
+      [7, 0]
+    ]
+    result = TestStudyRubyTopologicalSortGem::TestTopologicalSort.exec(graph)
+    assert(result == [4, 2, 1, 6, 3, 7, 0, 5])
+  end
+
+  def test_exec2
+    graph = [
+      [0, 5],
+      [1, 3],
+      [1, 6],
+      [2, 5],
+      [2, 7],
+      [3, 0],
+      [3, 7],
+      [4, 1],
+      [4, 2],
+      [4, 6],
+      [6, 7],
+      [7, 0]
+    ]
+    result = TestStudyRubyTopologicalSortGem::TestTopologicalSort.exec(graph)
+    assert(result == [4, 2, 1, 6, 3, 7, 0, 5])
+  end
+
+  def test_exec3
+    graph = [
+      [4, 2],
+      [3, 2],
+      [1, 2]
+    ]
+    result = TestStudyRubyTopologicalSortGem::TestTopologicalSort.exec(graph)
+    assert(result == [4, 2, 1, 6, 3, 7, 0, 5])
   end
 end
